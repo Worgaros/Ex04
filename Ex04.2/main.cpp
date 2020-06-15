5685aa3c1bc561eb551f7fcf46b78418b5935032
William <williamp10022000@gmail.com>
2020-06-15 14:34:31 +0200

    Ex04.2 completed

--- Ex04.2/main.cpp
+++ Ex04.2/main.cpp
@@ -7,10 +7,38 @@ En c++ : En considérant les termes de Fibonacci qui ont une valeurs qui n’est pa
 trouver la sommes des termes qui ont une valeurs pair.
 */
 
+#define MAX 4000000;
+
+int fib(unsigned int max, unsigned int result);
+
 int main()
 {
+	unsigned int result = 0;
+	const unsigned int max = MAX;
 
+	result = fib(max, result);
+
+	std::cout << "Sum of even fibonacci terms lower than 4000000: " << result << "\n";
 
 	system("pause");
 	return EXIT_SUCCESS;
+}
+
+int fib(unsigned int max, unsigned int result)
+{
+	unsigned int n = 0;
+	unsigned int nMoinsDeux = 0;
+	unsigned int nMoinsUn = 1;
+
+	for (unsigned int i = 2; i < max; i++)
+	{
+		n = nMoinsDeux + nMoinsUn;
+		if (n % 2 == 0)
+		{
+			result += n;
+		}
+		nMoinsDeux = nMoinsUn;
+		nMoinsUn = n;
+	}
+	return result;
 }
