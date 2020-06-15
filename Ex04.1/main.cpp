b2e312c6709831d1cb80d0faf9378ad7235b4e98
William <williamp10022000@gmail.com>
2020-06-15 14:49:35 +0200

    Ex04.1 and Ex04.2 corrected

--- Ex04.1/main.cpp
+++ Ex04.1/main.cpp
@@ -1,19 +1,27 @@
-#include <iostream>;
+#include <iostream>
+#define MAX_NUMBER 1000;
 
 int main()
 {
-	const int maxNum = 1000;
-	const int multiple1 = 3;
-	const int multiple2 = 5;
-	int sumMultiples = 0;
+	const int maxNumber = MAX_NUMBER;
+	int multipleOf3 = 0;
+	int multipleOf5 = 0;
+	int result = 0;
 
-	for (int i = 1; i < maxNum; i++)
+	for (int i = 0; i < maxNumber; i += 3)
 	{
-		if (i % multiple1 == 0 || i % multiple2 == 0)
-			sumMultiples += i;
+		multipleOf3 += i;
 	}
 
-	std::cout << "The sum of the multiples of 3 and 5 under " << maxNum << " is " << sumMultiples << ".\n\n";
+
+	for (int i = 0; i < maxNumber; i += 5)
+	{
+		multipleOf5 += i;
+	}
+
+	result = multipleOf3 + multipleOf5;
+
+	std::cout << "The sum of the multiples of 3 or 5 less than 1000 is " << result << ".\n\n";
 
 	system("pause");
 	return EXIT_SUCCESS;
--- Ex04.2/main.cpp
+++ Ex04.2/main.cpp
@@ -1,12 +1,5 @@
 #include <iostream>
 
-/*La suite de fibonacci est créée comme cela, chaque nouveau terme est égale à la somme des 2 précédents. 
-En prenant les 10 premier termes et en commençant par 1, 2 on obtient:
-1, 2, 3, 5, 8, 13, 21, 34, 55, 89
-En c++ : En considérant les termes de Fibonacci qui ont une valeurs qui n’est pas plus grande que 4 millions, 
-trouver la sommes des termes qui ont une valeurs pair.
-*/
-
 #define MAX 4000000;
 
 int fib(unsigned int max, unsigned int result);
@@ -18,7 +11,7 @@ int main()
 
 	result = fib(max, result);
 
-	std::cout << "Sum of even fibonacci terms lower than 4000000: " << result << "\n";
+	std::cout << "Sum of even fibonacci terms lower than 4000000: " << result << ".\n\n";
 
 	system("pause");
 	return EXIT_SUCCESS;
