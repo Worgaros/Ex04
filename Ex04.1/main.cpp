df31b9ac7aa882d4e5c21b49082bef4b4f934828
William <williamp10022000@gmail.com>
2020-06-15 17:20:21 +0200

    Added Ex04.3

--- Ex04.1/main.cpp
+++ Ex04.1/main.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
-#define MAX_NUMBER 1000;
+
+#define MAX_NUM 1000
+#define MULTIPLE_1 3
+#define MULTIPLE_2 5
 
 int main()
 {
-	const int maxNumber = MAX_NUMBER;
-	int multipleOf3 = 0;
-	int multipleOf5 = 0;
-	int result = 0;
+	int sumMultiples = 0;
+	const int maxNum = MAX_NUM;
+	const int multiple1 = MULTIPLE_1;
+	const int multiple2 = MULTIPLE_2;
 
-	for (int i = 0; i < maxNumber; i += 3)
+	for (int i = 1; i < maxNum; i++)
 	{
-		multipleOf3 += i;
+		if (i % multiple1 == 0 || i % multiple2 == 0)
+			sumMultiples += i;
 	}
 
-
-	for (int i = 0; i < maxNumber; i += 5)
-	{
-		multipleOf5 += i;
-	}
-
-	result = multipleOf3 + multipleOf5;
-
-	std::cout << "The sum of the multiples of 3 or 5 less than 1000 is " << result << ".\n\n";
+	std::cout << "The sum of the multiples of 3 or 5 less than 1000 is " << sumMultiples << ".\n\n";
 
 	system("pause");
 	return EXIT_SUCCESS;
---
+++ Ex04.3/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#define NUMBER 13195;
+
+int main()
+{
+	const int number = NUMBER;
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
