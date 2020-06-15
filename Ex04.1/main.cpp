981c7abd0656236978b369c05ab29e733d24b966
William <williamp10022000@gmail.com>
2020-06-15 13:59:57 +0200

    Ex04.1 completed

--- Ex04.1/main.cpp
+++ Ex04.1/main.cpp
@@ -2,7 +2,18 @@
 
 int main()
 {
+	const int maxNum = 1000;
+	const int multiple1 = 3;
+	const int multiple2 = 5;
+	int sumMultiples = 0;
 
+	for (int i = 1; i < maxNum; i++)
+	{
+		if (i % multiple1 == 0 || i % multiple2 == 0)
+			sumMultiples += i;
+	}
+
+	std::cout << "The sum of the multiples of 3 and 5 under " << maxNum << " is " << sumMultiples << ".\n\n";
 
 	system("pause");
 	return EXIT_SUCCESS;
