6ded4b66d1b1161981191af52ba7045a374d0d8f
William <williamp10022000@gmail.com>
2020-06-15 14:08:39 +0200

    Added Ex04.2

---
+++ Ex04.2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/*La suite de fibonacci est créée comme cela, chaque nouveau terme est égale à la somme des 2 précédents. 
+En prenant les 10 premier termes et en commençant par 1, 2 on obtient:
+1, 2, 3, 5, 8, 13, 21, 34, 55, 89
+En c++ : En considérant les termes de Fibonacci qui ont une valeurs qui n’est pas plus grande que 4 millions, 
+trouver la sommes des termes qui ont une valeurs pair.
+*/
+
+int main()
+{
+
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
