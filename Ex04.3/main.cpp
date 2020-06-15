73ed26e09102d05b4b0aff4405e887bf16de7096
William <williamp10022000@gmail.com>
2020-06-15 18:07:45 +0200

    Ex04.3 corrected

--- Ex04.3/main.cpp
+++ Ex04.3/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 
-#define NUMBER 13195;
+#define MAX_NUMBER 600851475143;
 
 int main()
 {
-	const int number = NUMBER;
+	const unsigned int maxNumber = MAX_NUMBER;
+	int biggestPrimeFactor = 0;
+	int factor = 1;
+
+	for (int i = 1; i < maxNumber; i++)
+	{
+		if (maxNumber % i == 0 && i % 1 == 0 && i % i == 0 && i > biggestPrimeFactor && factor < maxNumber)
+		{
+			factor *= i;
+			biggestPrimeFactor = i;
+		}
+	}
+
+	std::cout << "The biggest prime factor of 600 851 475 143 is " << biggestPrimeFactor << ".\n\n";
 
 	system("pause");
 	return EXIT_SUCCESS;
